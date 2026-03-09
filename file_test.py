2829f93cb1be2d774c2fe3b0e95a39677896c1a2
Ubuntu <ubuntu@ip-172-31-32-27.eu-west-1.compute.internal>
2026-03-09 16:23:06 +0000

    test the function

--- file_test.py
+++ file_test.py
@@ -1,17 +1,17 @@
-def  test_calc_addition():
-    output = 2+4
+def test_calc_addition():
+    output = 2 + 4
     assert output == 6
 
 
 def test_calc_substraction():
     # Function test the output of 2-4
-    output = 2-4
+    output = 2 - 4
     assert output == -2
 
 
 def test_calc_multiply():
     # Function test the output of 2*4
-    output = 2*4
+    output = 2 * 4
     assert output == 8
 
 
